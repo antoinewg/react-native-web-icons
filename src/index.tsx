4b2664b72beb31f799b7acd540cc3256194cf746
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-12-05 13:07:14 +0100

    chore: update dependencies and fix uuid

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,5 @@
+import 'react-native-get-random-values'; // required for `uuid` module to work
+
 export function multiply(a: number, b: number): Promise<number> {
   return Promise.resolve(a * b);
 }
